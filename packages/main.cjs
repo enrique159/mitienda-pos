4d27ca054971f6ad5a1682817c04bc94dc260764
Enrique Marin <enrique_marin_h@hotmail.com>
2025-01-02 10:40:45 -0700

    initialize Vue 3 + TypeScript + Vite project structure

---
+++ packages/main.cjs
@@ -0,0 +1,82 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+const env = require('./env.json')
+// const initDB = require('./app/database/index.cjs')
+const dev = env.NODE_ENV === 'development'
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true'
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 940,
+    minHeight: 620,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      nodeIntegration: true,
+      contextIsolation: true,
+      nodeIntegrationInWorker: true,
+    },
+  })
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  mainWindow.loadURL(
+    dev
+      ? 'http://localhost:8080'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  )
+  // Open the DevTools.
+  if (dev) {
+    console.log('[electron]: Opening DevTools')
+    mainWindow.webContents.openDevTools()
+  }
+}
+
+
+// if (dev) {
+//   const { default: installExtension, VUEJS_DEVTOOLS } = require('electron-devtools-installer');
+//   app.whenReady().then(() => {
+//     installExtension(VUEJS_DEVTOOLS)
+//       .then((name) => console.log(`[electron]: Added Extension:  ${name}`))
+//       .catch((err) => console.log('[electron]: An error occurred: ', err));
+//   });
+// }
+
+app.whenReady().then(() => {
+  // initDB().then(() => {
+  //   createWindow()
+  // })
+  createWindow()
+
+  app.on('activate', function() {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+ipcMain.on('close_app', () => {
+  app.quit()
+})
+
+ipcMain.on('restart_app', () => {
+  app.relaunch()
+  app.quit()
+})
+
+ipcMain.on('get_version', (event) => {
+  event.reply('get_version', app.getVersion())
+})
+
+// require('./app/modules/auth/authApplication.cjs')
+// require('./app/modules/areas/areasApplication.cjs')
+// require('./app/modules/actors/actorsApplication.cjs')
+// require('./app/modules/documents/documentsApplication.cjs')
+// require('./app/modules/activities/activitiesApplication.cjs')
+// require('./app/modules/configuration/configurationApplication.cjs')
---
+++ packages/preload.cjs
@@ -0,0 +1,54 @@
+const { contextBridge, ipcRenderer } = require('electron')
+// const { signIn, signUp, changePassword } = require('./app/modules/auth/authListeners.cjs')
+// const { getAreas, createArea, updateArea, deleteArea } = require('./app/modules/areas/areasListeners.cjs')
+// const { getActivities, createActivity, deleteActivity } = require('./app/modules/activities/activitiesListeners.cjs')
+// const { getActors, getActorByNombre, createActor, deleteActor } = require('./app/modules/actors/actorsListeners.cjs')
+// const { createDocument, getDocuments, openDocument, deleteDocument } = require('./app/modules/documents/documentsListeners.cjs')
+// const { getConfiguration, updateConfiguration, createConfiguration, exportDatabase, importDatabase, getVersion } = require('./app/modules/configuration/configurationListeners.cjs')
+
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
+
+// contextBridge.exposeInMainWorld('electron', {
+//   // Configuration
+//   getConfiguration,
+//   updateConfiguration,
+//   createConfiguration,
+//   exportDatabase,
+//   importDatabase,
+//   getVersion,
+//   // Auth
+//   signIn,
+//   signUp,
+//   changePassword,
+//   // Areas
+//   getAreas,
+//   createArea,
+//   updateArea,
+//   deleteArea,
+//   // Actors
+//   getActors,
+//   getActorByNombre,
+//   createActor,
+//   deleteActor,
+//   // Documents
+//   getDocuments,
+//   createDocument,
+//   openDocument,
+//   deleteDocument,
+//   // Activities
+//   getActivities,
+//   createActivity,
+//   deleteActivity,
+//   // Extras
+//   closeApp: () => ipcRenderer.send('close_app'),
+//   restartApp: () => ipcRenderer.send('restart_app'),
+// })
