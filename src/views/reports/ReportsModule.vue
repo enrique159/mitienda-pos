b450efd01a4116767604e5fa828ed48d21e653e5
Enrique Marin Hirales <enrique_marin_h@hotmail.com>
2025-10-17 09:36:24 -0700

    feat: add side menu and routing structure to reports module

--- src/views/reports/ReportsModule.vue
+++ src/views/reports/ReportsModule.vue
@@ -1,13 +1,26 @@
 <template>
-  <div>
-    Reportes
+  <div class="w-full h-full reports-grid">
+    <side-menu :items-menu="reportsMenu" />
+    <router-view />
   </div>
 </template>
 
 <script setup lang="ts">
+import SideMenu from '@/components/menus/SideMenu.vue'
+import { IconListCheck } from '@tabler/icons-vue'
 
+const reportsMenu = [
+  {
+    title: 'Reportes',
+    path: '/main/reports',
+    icon: IconListCheck,
+  },
+]
 </script>
 
 <style scoped>
-
+.reports-grid {
+  display: grid;
+  grid-template-columns: 240px 1fr;
+}
 </style>
--- src/views/reports/routes.ts
+++ src/views/reports/routes.ts
@@ -1,9 +1,17 @@
 import ReportsModule from './ReportsModule.vue'
+import ReportsView from './views/ReportsView.vue'
 
 export const ReportsRoutes = [
   {
     path: '/main/reports',
     name: 'Reports',
     component: ReportsModule,
+    children: [
+      {
+        path: '',
+        name: 'ReportsView',
+        component: ReportsView,
+      },
+    ],
   },
 ]
