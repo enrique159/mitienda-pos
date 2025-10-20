8aba1f91b7a92a1b6cf5c2f6145e9aea0da7aa5c
carlosadansc <charliecesena.94@gmail.com>
2025-10-20 10:30:34 -0700

    feat: add inventories & inventory_items tables

---
+++ packages/app/database/schemas/inventory_items.cjs
@@ -0,0 +1,25 @@
+const { logger } = require('../../helpers/index.cjs')
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.createTable = async function(knex) {
+  await knex.schema.createTable('inventory_items', (table) => {
+    table.uuid('id').defaultTo(knex.fn.uuid()).primary() // Llave primaria
+    table.uuid('id_inventory').notNullable().references('iventories.id') // A que inventario pertenece
+    table.uuid('id_product').notNullable().references('products.id') // A que producto pertenece
+    table.number('counted_quantity').notNullable()
+    table.number('registered_quantity').notNullable()
+    table.string('incidence').notNullable()
+    table.string('note').nullable().defaultTo('')
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
+    table.timestamp('synced_at')
+  }).then(() => {
+    console.log("Table 'inventory_items' created.")
+  }).catch((err) => {
+    logger.error({ type: 'DB', error: err })
+    console.error(err)
+  })
+}
